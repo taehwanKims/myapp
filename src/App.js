031728cbcf3f525035dafafafcecf5bf9006a75c
KIM <xoghks421@naver.com>
2019-04-17 16:10:47 +0900

    added readme

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PhonForm from './components/PhoneForm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       <PhonForm/>
+      </div>
+    );
+  }
+}
+
+export default App;
